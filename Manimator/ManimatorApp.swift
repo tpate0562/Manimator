9ad8c9167d64c2100fdabeeaaaff8a27df5cd4ab
tpate0562 <56415907+tpate0562@users.noreply.github.com>
2026-03-26 17:26:43 -0700

    Initial Commit

---
+++ Manimator/ManimatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManimatorApp.swift
+//  Manimator
+//
+//  Created by Tejas Patel on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct ManimatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ManimatorTests/ManimatorTests.swift
@@ -0,0 +1,19 @@
+//
+//  ManimatorTests.swift
+//  ManimatorTests
+//
+//  Created by Tejas Patel on 3/26/26.
+//
+
+import Testing
+@testable import Manimator
+
+struct ManimatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
